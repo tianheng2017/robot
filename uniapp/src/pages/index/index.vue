caac90be9575e757d312a364570db861803e47d6
13764074891 <1397478644@qq.com>
2023-02-06 17:05:20 +0800

    '更新'

--- uniapp/src/pages/index/index.vue
+++ uniapp/src/pages/index/index.vue
@@ -1,9 +1,7 @@
 <template>
 	<view>
 		<van-config-provider :theme="appStore.theme" :theme-vars="themeVars">
-			<view class="absolute right-5 top-5" @click="appStore.change()">
-				<van-image :src="appStore.icon" width="35" position="right" />
-			</view>
+			<view class="absolute right-5 top-5" @click="appStore.change()"><van-image :src="appStore.icon" width="35" position="right" /></view>
 			<view class="py-5 text-center">
 				<van-image width="60" src="/static/logo.png" />
 				<view class="light text-4xl mt-2">周易量化</view>
@@ -115,118 +113,139 @@
 					</van-tab>
 					<van-tab title="运行日志">
 						<van-cell-group>
-							<block v-for="(v, i) in appStore.data.list" :key="i">
-								<van-cell :border="false" :title-class="i ? '' : 'text-red mt-2.5'" :title="v" />
-							</block>
+							<block v-for="(v, i) in appStore.data.list" :key="i"><van-cell :border="false" :title-class="i ? '' : 'text-red mt-2.5'" :title="v" /></block>
 						</van-cell-group>
 					</van-tab>
 					<van-tab title="基础配置">
 						<van-form @submit="onSubmit">
 							<van-cell-group class="pt-2.5">
-								<van-field v-model="formData.firstOrder" type="number" name="首单数量" label="首单数量"
-									placeholder="首单数量" :rules="[{ required: true, message: '请填写首单数量' }]" />
-								<van-field v-model="formData.allAddPositionNum" type="digit" name="最大补仓" label="最大补仓"
-									placeholder="最大补仓次数" :rules="[{ required: true, message: '请填写最大补仓次数' }]" />
-								<van-field v-model="formData.lever" type="digit" name="杠杆倍数" label="杠杆倍数"
-									placeholder="杠杆倍数" :rules="[{ required: true, message: '请填写杠杆倍数' }]" />
-								<van-field v-model="formData.profitRatio" type="number" name="止盈率" label="止盈率(%)"
-									placeholder="止盈率" :rules="[{ required: true, message: '请填写止盈率' }]" />
-								<van-field v-model="formData.stopLossRatio" type="number" name="止损率" label="止损率(%)"
-									placeholder="止损率" :rules="[{ required: true, message: '请填写止损率' }]" />
+								<van-field
+									v-model="formData.firstOrder"
+									type="number"
+									name="首单数量"
+									label="首单数量"
+									placeholder="首单数量"
+									:rules="[{ required: true, message: '请填写首单数量' }]"
+								/>
+								<van-field
+									v-model="formData.allAddPositionNum"
+									type="digit"
+									name="最大补仓"
+									label="最大补仓"
+									placeholder="最大补仓次数"
+									:rules="[{ required: true, message: '请填写最大补仓次数' }]"
+								/>
+								<van-field
+									v-model="formData.lever"
+									type="digit"
+									name="杠杆倍数"
+									label="杠杆倍数"
+									placeholder="杠杆倍数"
+									:rules="[{ required: true, message: '请填写杠杆倍数' }]"
+								/>
+								<van-field
+									v-model="formData.profitRatio"
+									type="number"
+									name="止盈率"
+									label="止盈率(%)"
+									placeholder="止盈率"
+									:rules="[{ required: true, message: '请填写止盈率' }]"
+								/>
+								<van-field
+									v-model="formData.stopLossRatio"
+									type="number"
+									name="止损率"
+									label="止损率(%)"
+									placeholder="止损率"
+									:rules="[{ required: true, message: '请填写止损率' }]"
+								/>
 							</van-cell-group>
-							<div style="margin: 16px;">
-								<van-button plain round block type="success" native-type="submit">
-									提交
-								</van-button>
-							</div>
+							<div style="margin: 16px;"><van-button plain round block type="success" native-type="submit">提交</van-button></div>
 						</van-form>
 					</van-tab>
 				</van-tabs>
 				<van-back-top />
-				<view class="text-center text-title2 text-sm fixed bottom-2 right-0 left-0">
-					在线：{{ appStore.data.other.online || 0}} 人</view>
+				<view class="text-center text-title2 text-sm fixed bottom-2 right-0 left-0">在线：{{ appStore.data.other.online || 0 }} 人</view>
 			</view>
 		</van-config-provider>
 	</view>
 </template>
 <script setup>
-	import {
-		onUnload
-	} from '@dcloudio/uni-app';
-	import {
-		computed,
-		onMounted,
-		reactive,
-		ref
-	} from 'vue'
-	import {
-		showToast
-	} from 'vant'
-	import useAppStore from '@/store/modules/app'
+import { onLoad, onUnload } from '@dcloudio/uni-app';
+import { computed, onMounted, reactive, ref } from 'vue';
+import { showLoadingToast, showToast } from 'vant';
+import useAppStore from '@/store/modules/app';
 
-	// 状态数据
-	const appStore = useAppStore()
+// 状态数据
+const appStore = useAppStore();
 
-	// 组件样式定制
-	const themeVars = reactive({
-		cellLineHeight: '10px'
-	})
+// 组件样式定制
+const themeVars = reactive({
+	cellLineHeight: '10px'
+});
 
-	// 表单数据
-	const formData = reactive({
-		'firstOrder': appStore.data.other.firstOrder,
-		'allAddPositionNum': appStore.data.other.allAddPositionNum,
-		'lever': appStore.data.other.lever,
-		'profitRatio': appStore.data.other.profitRatio,
-		'stopLossRatio': appStore.data.other.stopLossRatio,
-	})
-	// 表单事件
-	const onSubmit = () => {
-		showToast('开发中')
-	}
+// 表单数据
+const formData = reactive({
+	firstOrder: appStore.data.other.firstOrder,
+	allAddPositionNum: appStore.data.other.allAddPositionNum,
+	lever: appStore.data.other.lever,
+	profitRatio: appStore.data.other.profitRatio,
+	stopLossRatio: appStore.data.other.stopLossRatio
+});
+// 表单事件
+const onSubmit = () => {
+	showToast('开发中');
+};
 
-	// 创建webSocket
-	uni.connectSocket({
-		url: `${import.meta.env.VITE_APP_BASE_URL || ''}`
-	})
-	// 打开webSocket
-	uni.onSocketOpen(function(res) {
-		console.log('webSocket连接成功')
-		// 发送ping
-		uni.sendSocketMessage({
-			data: 'ping'
-		})
-		// 接收数据
-		uni.onSocketMessage(function(res) {
-			// 格式化数据
-			res = JSON.parse(res.data)
-			// 获取运行日志
-			appStore.data.list = res.data.list
-			// 获取其他数据
-			appStore.data.other = res.data.other
-		})
-	})
-	// 打开webSocket失败
-	uni.onSocketError(function(res) {
-		console.log('webSocket连接失败')
-	})
+// 创建webSocket
+uni.connectSocket({
+	url: `${import.meta.env.VITE_APP_BASE_URL || ''}`
+});
+// 加载层
+const loading = showLoadingToast({
+	message: '加载中...',
+	forbidClick: true
+});
+// 打开webSocket
+uni.onSocketOpen(function(res) {
+	console.log('webSocket连接成功');
+	// 发送ping
+	uni.sendSocketMessage({
+		data: 'ping'
+	});
+	// 接收数据
+	uni.onSocketMessage(function(res) {
+		// 格式化数据
+		res = JSON.parse(res.data);
+		// 获取运行日志
+		appStore.data.list = res.data.list;
+		// 获取其他数据
+		appStore.data.other = res.data.other;
+	});
+	loading.close();
+});
+// 打开webSocket失败
+uni.onSocketError(function(res) {
+	console.log('webSocket连接失败');
+	loading.close();
+});
 
-	// 离开页面关闭websocket
-	onUnload(() => {
-		uni.closeSocket()
-	})
+// 离开页面关闭websocket
+onUnload(() => {
+	uni.closeSocket();
+});
 
-	// 图标预加载
-	onMounted(() => {
-		appStore.preload()
-	})
+// 图标预加载
+onMounted(() => {
+	appStore.preload();
+});
 </script>
 <style scoped>
-	.van-theme-dark body {
-		background-color: black;
-	}
+.van-theme-dark body {
+	background-color: black;
+}
 
-	.van-theme-dark body .light {
-		color: #fff;
-	}
+.van-theme-dark body .light {
+	color: #fff;
+}
 </style>
